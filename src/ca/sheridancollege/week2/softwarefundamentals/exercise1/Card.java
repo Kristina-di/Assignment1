e4af704ea7b36837a05116912eec994232dc3436
Kristina-di <62858565+Kristina-di@users.noreply.github.com>
2020-06-07 17:05:41 -0400

    Update Card.java

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/Card.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week2.softwarefundamentals.exercise1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author Kristina Dias
+ * date: 07/06/2020
  */
 public class Card {
 
